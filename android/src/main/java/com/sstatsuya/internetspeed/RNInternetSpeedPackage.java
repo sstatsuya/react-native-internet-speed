6d079467bddc1f6d4f7288188c3fcfad8bc2bb39
luongminhtien <tienluongminh@thegioididong.com>
2024-03-05 14:20:44 +0700

    fix package name

--- android/src/main/java/com/sstatsuya/internetspeed/RNInternetSpeedPackage.java
+++ android/src/main/java/com/sstatsuya/internetspeed/RNInternetSpeedPackage.java
@@ -13,7 +13,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RNInternetSpeedPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNNetworkSpeedModule(reactContext));
+      return Arrays.<NativeModule>asList(new RNInternetSpeedModule(reactContext));
     }
 
     // Deprecated from RN 0.47
